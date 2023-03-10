15bfd2e5d623948f986922d60afda8a1868ab761
samstrathman <117926965+samstrathman@users.noreply.github.com>
2023-03-10 08:36:20 -0500

    added my solution to the project

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+//sam's added code to complete freeCodeCamp project
+app.get('/api/whoami', (req, res) => {
+  const ipAddress = req.connection.remoteAddress;
+  const language = req.headers["accept-language"];
+  const software = req.headers["user-agent"];
+  res.json({
+    ipaddress: ipAddress,
+    language: language,
+    software: software
+  })
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
